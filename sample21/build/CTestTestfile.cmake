72a24903a47601dfacd08f7f7964205ea956c5d8
rookie <2890023401@qq.com>
2021-09-16 00:37:01 +0800

    remove build

--- sample21/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lovefantasy/Desktop/LearnOpenGL/sample21
-# Build directory: /home/lovefantasy/Desktop/LearnOpenGL/sample21/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("stb")
