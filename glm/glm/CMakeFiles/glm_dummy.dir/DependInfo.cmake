96a0c735d14f9538c6b4f4f0e1af709d700955eb
rookie <2890023401@qq.com>
2023-10-10 01:41:24 +0800

    rebase

---
+++ glm/glm/CMakeFiles/glm_dummy.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lovefantasy/Desktop/LearnOpenGL/glm/glm/detail/dummy.cpp" "glm/CMakeFiles/glm_dummy.dir/detail/dummy.cpp.o" "gcc" "glm/CMakeFiles/glm_dummy.dir/detail/dummy.cpp.o.d"
+  "/home/lovefantasy/Desktop/LearnOpenGL/glm/glm/detail/glm.cpp" "glm/CMakeFiles/glm_dummy.dir/detail/glm.cpp.o" "gcc" "glm/CMakeFiles/glm_dummy.dir/detail/glm.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ glm/test/core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lovefantasy/Desktop/LearnOpenGL/glm/test/core
+# Build directory: /home/lovefantasy/Desktop/LearnOpenGL/glm/test/core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
